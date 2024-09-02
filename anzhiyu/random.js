ec79cb8a5c007a75b8a62301ba409b31b38e360c
mujiwanma nan <nanmu@kingsoft.com>
2024-09-02 15:48:59 +0800

    Site updated: 2024-09-02 15:48:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/02/hello-world/"];function toRandomPost(){
+var posts=["2024/09/02/hello-world/","2024/09/02/java面经/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
